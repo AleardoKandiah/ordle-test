71a7a77e853f557b04c2cfc6042b73a840a9291f
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-27 14:49:36 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  // API key
+  useEffect(() => {
+    Cohere.init("1MH_");
+  }, [])
   return (
 
 
