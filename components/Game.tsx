ccdee0e6bfac4f77aca22167c5ecce09869cc7e4
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-27 15:20:15 +0100

    game commit

---
+++ components/Game.tsx
@@ -0,0 +1,12 @@
+export type Accuracy =
+    | "right" //Right letter and right position
+    | "wrong" //Wrong letter
+    | "almost" //Right letter and wrong position
+    | "unknown" //Not evaluated
+
+const getRandomCommonWord = () =>
+    commonWordsArray[Math.floor(Math.random () * commonWordsArray.length)];
+
+export const Game = () => {
+    const { solutionWord, setSolutionWord} = useContext(SolutionWordContext)
+}
