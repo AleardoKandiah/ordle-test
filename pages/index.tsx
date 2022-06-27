1a98e7f64f70818cb3e683b5ab1afa5ff6dbc4a1
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-27 15:12:46 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,22 @@ const Home: NextPage = () => {
   useEffect(() => {
     Cohere.init("1MH_");
   }, [])
+  
   return (
-
+   <div>
+     <Head>
+      <title>ordle</title>
+      <meta name='description' content='Guess the secret word '/>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name='viewport' content='width-device-width, initial-scale=1'/>
+     </Head>
+    <main>
+      <SolutionWordProvider>
+        <Nav/>
+        <Game/>
+      </SolutionWordProvider>
+    </main>
+   </div>
 
      
   );
