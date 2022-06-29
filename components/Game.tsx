3e0416d5c7df84fea69afd750739b9c5c1c9130b
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-29 19:18:49 +0100

    initial commit

--- components/Game.tsx
+++ components/Game.tsx
@@ -56,7 +56,7 @@ const onSubmit = useCallBack(() => {
                 const isKsyWrong=
                 accuracies[i] === "wrong" &&
                 // Not previously guessed
-                !guessedLetters.some(letter) => le
+                !guessedLetters.some(letter) => let
             }
         }
     }
