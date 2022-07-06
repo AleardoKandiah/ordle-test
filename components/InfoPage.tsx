7749eab507ef2fd4073db249903a35feda12da82
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-06 19:53:43 +0100

    commit

--- components/InfoPage.tsx
+++ components/InfoPage.tsx
@@ -6,5 +6,5 @@ const CopyLink = () {
     const {solutionWord} = useContext(SolutionWordContext);
     const link = `${window.location.href}?word=${encodedURIComponent(
         Buffer.from(solutionWord).toString("base64")
-    )}`
+    )}`;
 }
