cbc17547faa8ea4c119267dd8de866d11d7e8d5c
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-06 19:54:51 +0100

    buffer

--- components/InfoPage.tsx
+++ components/InfoPage.tsx
@@ -5,6 +5,7 @@ import { useContext, useEffect, useState } from "react";
 const CopyLink = () {
     const {solutionWord} = useContext(SolutionWordContext);
     const link = `${window.location.href}?word=${encodedURIComponent(
+        // buffer for base64 encoding
         Buffer.from(solutionWord).toString("base64")
     )}`;
 }
