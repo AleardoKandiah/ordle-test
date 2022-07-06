7dec512eb04c1dcee58a88a64f30cd6d4970b5f9
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-06 19:53:34 +0100

    solution word ref

--- components/InfoPage.tsx
+++ components/InfoPage.tsx
@@ -3,5 +3,8 @@ import { useContext, useEffect, useState } from "react";
 
 
 const CopyLink = () {
-    const {solutionWord} = useContext(solutionWordContex);
+    const {solutionWord} = useContext(SolutionWordContext);
+    const link = `${window.location.href}?word=${encodedURIComponent(
+        Buffer.from(solutionWord).toString("base64")
+    )}`
 }
