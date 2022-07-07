64b59beb7d691087e0a73b6a997b72030cabe398
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-07-07 20:19:49 +0100

    commit

--- components/InfoPage.tsx
+++ components/InfoPage.tsx
@@ -8,16 +8,17 @@ const CopyLink = () {
         // buffer for base64 encoding into base64
         Buffer.from(solutionWord).toString("base64")
     )}`;
+
+
+    const [recentlyCopied, setRecentlyCopied] = useState(false);
+
+    // check icon reset to copy icon after 1s
+    useEffect(() => {
+        if(recentlyCopied) {
+            const timeout = setTimeout(() => {
+                setRecentlyCopied(flase);
+            }, 1000);
+            return () => clearTimeout(timeout);
+        }
+    })
 }
-
-const [recentlyCopied, setRecentlyCopied] = useState(false);
-
-// check icon reset to copy icon after 1s
-useEffect(() => {
-    if(recentlyCopied) {
-        const timeout = setTimeout(() => {
-            setRecentlyCopied(flase);
-        }, 1000);
-        return () => clearTimeout(timeout);
-    }
-})
